cae0fa5a258f91c4c0d3415d09896d41ab2bd653
a1055889855 <1055889855@qq.com>
2020-12-01 17:38:47 +0800

    第一次上传

---
+++ servent-shen-boot/src/main/java/com/serventshen/serventshenboot/ServentShenBootApplication.java
@@ -0,0 +1,13 @@
+package com.serventshen.serventshenboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServentShenBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServentShenBootApplication.class, args);
+    }
+
+}
---
+++ servent-shen-boot/src/test/java/com/serventshen/serventshenboot/ServentShenBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serventshen.serventshenboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServentShenBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
